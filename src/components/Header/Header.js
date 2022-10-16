142fc9c794daee487379b432aa5184b897a1c97b
sajid365-sr <sajidsorker86@gmail.com>
2022-10-16 22:46:05 +0600

    basic router setup with header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav>
+            <Link to='/home'>Home</Link>
+        </nav>
+    );
+};
+
+export default Header;
